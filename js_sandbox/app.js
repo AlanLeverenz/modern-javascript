7de2267c827488e72e148e0f35a20f3a7635a43c
Alan Leverenz <awleverenz@aol.com>
2020-06-29 13:53:59 -0400

    data types

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,1 +1,33 @@
-alert('Hello World');
+// Const
+
+const person = {
+  name: 'John',
+  age: 30,
+};
+
+// can mutate but not reassign a const
+person.name = 'Sara';
+person.age = 42;
+
+console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+
+// Primitive Data Types:
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbols (ES6)
+
+// Reference Types
+
+// Arrays
+// Object Literals
+// Functions
+// Dates
+// Anything else
