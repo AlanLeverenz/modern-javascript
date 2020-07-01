8e5abb7ecc887565eb150322d27d7b23e92a156c
Alan Leverenz <awleverenz@aol.com>
2020-06-30 20:54:15 -0400

    string methods and concatenation

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,25 +1,57 @@
-const num1 = 100;
-const num2 = 50;
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello there my name is Brad';
+const tags = 'web design,web development,web programming';
+
 let val;
 
-// Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+val = firstName + ' ' + lastName;
 
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2, 33, 4, 1, 55, 6, -3);
-val = Math.max(2, 33, 4, 1, 55, 6, -3);
-val = Math.floor(Math.random() * 20 + 1);
+// Append
+val = 'Brad ';
+val += 'Traversy';
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = "That's awesome, I can't wait";
+
+// Length
+val = firstName.length;
+
+// Concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[0];
+
+// indexOf
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+// charAt()
+val = firstName.charAt(2);
+// get last char
+val = firstName.charAt(firstName.length - 1);
+
+// substring()
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// includes()
+val = str.includes('Hello');
 
 console.log(val);
