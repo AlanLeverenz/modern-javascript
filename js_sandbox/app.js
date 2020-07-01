cfc7093ba2d76dfd13f1283d590b4cc567169572
Alan Leverenz <awleverenz@aol.com>
2020-06-30 21:32:43 -0400

    String templates (literals)

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,57 +1,52 @@
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 36;
-const str = 'Hello there my name is Brad';
-const tags = 'web design,web development,web programming';
+const name = 'John';
+const age = 31;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
 
-let val;
+// without template strings  (ES5)
+html =
+  '<ul><li>Name: ' +
+  name +
+  '</li><li>Age: ' +
+  age +
+  '</li><li>Job: ' +
+  job +
+  '</li><li>City: ' +
+  city +
+  '</li></ul>';
 
-val = firstName + ' ' + lastName;
+html =
+  '<ul>' +
+  '<li>Name: ' +
+  name +
+  '</li>' +
+  '<li>Age: ' +
+  age +
+  '</li>' +
+  '<li>Job: ' +
+  job +
+  '</li>' +
+  '<li>City: ' +
+  city +
+  '</li>' +
+  '</ul>';
 
-// Append
-val = 'Brad ';
-val += 'Traversy';
-val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+function hello() {
+  return 'hello';
+}
 
-// Escaping
-val = "That's awesome, I can't wait";
+// with template strings (ES6) literals
+html = `
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+        <li>${2 + 2}</li>
+        <li>${hello()}</li>
+        <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+        </ul>
+  `;
 
-// Length
-val = firstName.length;
-
-// Concat
-val = firstName.concat(' ', lastName);
-
-// Change case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
-
-val = firstName[0];
-
-// indexOf
-val = firstName.indexOf('l');
-val = firstName.lastIndexOf('l');
-
-// charAt()
-val = firstName.charAt(2);
-// get last char
-val = firstName.charAt(firstName.length - 1);
-
-// substring()
-val = firstName.substring(0, 4);
-
-// slice()
-val = firstName.slice(0, 4);
-val = firstName.slice(-3);
-
-// split()
-val = str.split(' ');
-val = tags.split(',');
-
-// replace()
-val = str.replace('Brad', 'Jack');
-
-// includes()
-val = str.includes('Hello');
-
-console.log(val);
+document.body.innerHTML = html;
