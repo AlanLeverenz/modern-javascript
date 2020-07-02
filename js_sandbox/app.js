ce7105cc4b9049c70aea1200bb01dbb644fa8464
Alan Leverenz <awleverenz@aol.com>
2020-07-01 21:31:00 -0400

    GENERAL LOOPS

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,52 +1,75 @@
-// FUNCTION DECLARATIONS
+// LOOPS & ITERATIONS
 
-function greet(firstName = 'John', lastName = 'Doe') {
-  // if (typeof firstName === 'undefined') {
-  //   firstName = 'John';
-  // }
-  // if (typeof lastName === 'undefined') {
-  //   firstName = 'Doe';
-  // }
+// FOR LOOP
 
-  // console.log('Hello');
-  return 'Hello ' + firstName + ' ' + lastName;
+// for (let i = 0; i < 10; i++) {
+//   // console.log('Number ' + i);
+//   if (i === 2) {
+//     console.log('2 is my favorite number');
+//     continue;
+//   }
+
+//   if (i === 5) {
+//     console.log('Stop the loop');
+//     break;
+//   }
+//   console.log('Number ' + i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// DO WHILE
+
+// let i = 00;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// } while (i < 10);
+
+// LOOPING THROUGH ARRAY
+
+const cars = ['Ford', 'Chevy', 'Honda', 'Yoyota'];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// FOREACH
+// cars.forEach(function (car, index, array) {
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users = [
+//   { id: 1, name: 'John' },
+//   { id: 2, name: 'Sarah' },
+//   { id: 3, name: 'Karen' },
+// ];
+
+// const ids = users.map(function (user) {
+//   return user.id;
+// });
+
+// console.log(ids);
+
+// FOR IN LOOP
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 40,
+};
+
+// x gives the key, [x] gives the value
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
 }
-
-// console.log(greet());
-
-// FUNCTION EXPRESSION
-
-const square = function (x = 3) {
-  return x * x;
-};
-
-// console.log(square());
-
-// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
-
-// (function () {
-//   console.log('IIFE Ran..');
-// })();
-
-// (function (name) {
-//   console.log('Hello ' + name);
-// })('Brad');
-
-// PROPERTY METHODS (Function inside object is a method)
-
-const todo = {
-  add: function () {
-    console.log('Add todo..');
-  },
-  edit: function (id) {
-    console.log(`Edit todo ${id}`);
-  },
-};
-
-todo.delete = function () {
-  console.log('Delete todo..');
-};
-
-todo.add();
-todo.edit(22);
-todo.delete();
