ec3302ede8b364210bf51109ef02870c7f6e5b73
Alan Leverenz <awleverenz@aol.com>
2020-07-01 20:36:24 -0400

    function declarations, expressions, iife's, methods

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,43 +1,52 @@
-// SWITCHES
+// FUNCTION DECLARATIONS
 
-const color = 'red';
+function greet(firstName = 'John', lastName = 'Doe') {
+  // if (typeof firstName === 'undefined') {
+  //   firstName = 'John';
+  // }
+  // if (typeof lastName === 'undefined') {
+  //   firstName = 'Doe';
+  // }
 
-switch (color) {
-  case 'red':
-    console.log('Color is red');
-    break;
-  case 'blue':
-    console.log('Color is blue');
-    break;
-  default:
-    console.log('Color is not red or blue');
-    break;
+  // console.log('Hello');
+  return 'Hello ' + firstName + ' ' + lastName;
 }
 
-let day;
+// console.log(greet());
 
-switch (new Date().getDay()) {
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 2:
-    day = 'Tuesday';
-    break;
-  case 3:
-    day = 'Wednesday';
-    break;
-  case 4:
-    day = 'Thursday';
-    break;
-  case 5:
-    day = 'Friday';
-    break;
-  case 6:
-    day = 'Saturday';
-    break;
-}
+// FUNCTION EXPRESSION
 
-console.log(`Today is ${day}`);
+const square = function (x = 3) {
+  return x * x;
+};
+
+// console.log(square());
+
+// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+// (function () {
+//   console.log('IIFE Ran..');
+// })();
+
+// (function (name) {
+//   console.log('Hello ' + name);
+// })('Brad');
+
+// PROPERTY METHODS (Function inside object is a method)
+
+const todo = {
+  add: function () {
+    console.log('Add todo..');
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  },
+};
+
+todo.delete = function () {
+  console.log('Delete todo..');
+};
+
+todo.add();
+todo.edit(22);
+todo.delete();
