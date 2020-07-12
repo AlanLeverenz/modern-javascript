a035488bc715d0b55d3f813139025a2a9479cf6d
Alan Leverenz <awleverenz@aol.com>
2020-07-12 05:04:39 -0400

    added comments to Prototype Inheritance

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,27 +1,53 @@
-const personPrototypes = {
-  greeting: function () {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function (newLastName) {
-    this.lastName = newLastName;
-  },
+// Person constructor
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+// Greeting function assigned to prototype
+Person.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName}`;
 };
 
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+// create instance of Person constructor
+const person1 = new Person('John', 'Doe');
 
-console.log(mary);
-mary.getsMarried('Thompson');
-console.log(mary.greeting());
+console.log(person1.greeting());
 
-// insert object properties into the Object.create parameters
-const brad = Object.create(personPrototypes, {
-  firstName: { value: 'Brad' },
-  lastName: { value: 'Traversy' },
-  age: { value: 36 },
-});
+// Customer constructor, use 'call' method to include Person objects
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
 
-console.log(brad);
-console.log(brad.greeting());
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Inherit the Person prototype method 'greeting()' as new object
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer() by adding as constructor
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+// Customer greeting replaces Person greeting
+Customer.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company.`;
+};
+
+console.log(customer1);
+
+console.log(customer1.greeting());
+
+/*
+Customer {firstName: "Tom", lastName: "Smith", phone: "555-555-5555", membership: "Standard"}
+firstName: "Tom"
+lastName: "Smith"
+membership: "Standard"
+phone: "555-555-5555"
+__proto__: Person
+constructor: ƒ Customer(firstName, lastName, phone, membership)
+greeting: ƒ ()
+__proto__: Object
+*/
