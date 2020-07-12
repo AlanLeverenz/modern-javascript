be0bcaf0de85c95d28781c4d272410ce3ab57394
Alan Leverenz <awleverenz@aol.com>
2020-07-12 16:48:41 -0400

    ES6 Sub Classes

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,36 +1,30 @@
-// ES6 CLASSES
-// can insert functions inside the class
+// SUB CLASSES (INTERITANCE in ES6)
 class Person {
-  constructor(firstName, lastName, dob) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob);
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
+}
 
-  calculateAge() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+// create a sub class
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
   }
 
-  getsMarried(newLastName) {
-    this.lastName = newLastName;
-  }
-
-  // used without "this"
-  static addNumbers(x, y) {
-    return x + y;
+  static getMembershipCost() {
+    return 500;
   }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
 
-mary.getsMarried('Thompson');
-
-console.log(mary);
-
-console.log(Person.addNumbers(1, 2));
+console.log(john.greeting());
+console.log(Customer.getMembershipCost());
