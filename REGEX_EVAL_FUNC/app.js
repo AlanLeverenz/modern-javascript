49c6278e585430b61dae5970b3386bf0e5402a65
Alan Leverenz <awleverenz@aol.com>
2020-07-27 22:00:20 -0400

    Regex metacharacter symbols

--- REGEX_EVAL_FUNC/app.js
+++ REGEX_EVAL_FUNC/app.js
@@ -4,7 +4,7 @@ re = /hello/i; // i = case insensitive
 // re = /hello/g; // Global search
 
 // console.log(re);
-// console.log(re.source);
+console.log(re.source);
 
 // exec() - Return result in an array or null
 // const result = re.exec('brad hello world');
@@ -29,6 +29,6 @@ re = /hello/i; // i = case insensitive
 // console.log(result);
 
 // replace() - Return new string iwth some or all matches of a pattern
-const str = 'Hello There';
-const newStr = str.replace(re, 'Hi');
-console.log(newStr);
+// const str = 'Hello There';
+// const newStr = str.replace(re, 'Hi');
+// console.log(newStr);
---
+++ REGEX_METACHARS/app.js
@@ -0,0 +1,32 @@
+let re;
+
+// Literal characters
+re = /hello/;
+re = /hello/i;
+
+// Metacharacter Symbols
+re = /^h/i; // Must start with
+re = /World$/i; // Must end with
+re = /^hello$/i; // Must begin and end with
+re = /h.llo/i; // Matches any ONE character
+re = /h*llo/i; // Matches any ONE character (or none)
+re = /gra?e?y/i; // Optional character (or none)
+re = /gra?e?y/i; // Optional character (or none)
+re = /gra?e?y\?/i; // Optional character (or none)
+
+// String to match
+const str = 'Gray?';
+
+// Log results
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re, str) {
+  if (re.test(str)) {
+    console.log(`${str} matches ${re.source}`);
+  } else {
+    console.log(`${str} does NOT match $(re.source})`);
+  }
+}
+
+reTest(re, str);
